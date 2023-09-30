c775a9fa34668669989c0c3f69b55f2f67bf21b8
Infernus Scriptz <139264639+InfernusScripts@users.noreply.github.com>
2023-09-30 19:50:56 +0300

    Update InstancesInfo.lua

--- InstancesInfo.lua
+++ InstancesInfo.lua
@@ -1,3 +1,304 @@
+--if we will turn the returned table to a string, we will get ~96467 lines of code, and this is not a joke.
+--that thing is very useful for things like a explorers and etc.
+--[[
+Template structure:
+data.Part = {
+	["Properties"] = {
+		["className"] = {
+			["type"] = "string"
+		},
+		["archivable"] = {
+			["type"] = "bool"
+		},
+		["Name"] = {
+			["type"] = "string"
+		},
+		["ClassName"] = {
+			["type"] = "string"
+		},
+		["Parent"] = {
+			["type"] = "Instance"
+		},
+		["DataCost"] = {
+			["type"] = "int"
+		},
+		["Archivable"] = {
+			["type"] = "bool"
+		},
+		["RobloxLocked"] = {
+			["type"] = "bool"
+		}
+	},
+	["Events"] = {
+		["childAdded"] = {
+			["Arguments"] = {
+				[1] = {
+					["Name"] = "child",
+					["Type"] = "Instance"
+				}
+			}
+		},
+		["DescendantRemoving"] = {
+			["Arguments"] = {
+				[1] = {
+					["Name"] = "descendant",
+					["Type"] = "Instance"
+				}
+			}
+		},
+		["AncestryChanged"] = {
+			["Arguments"] = {
+				[1] = {
+					["Name"] = "child",
+					["Type"] = "Instance"
+				},
+				[2] = {
+					["Name"] = "parent",
+					["Type"] = "Instance"
+				}
+			}
+		},
+		["DescendantAdded"] = {
+			["Arguments"] = {
+				[1] = {
+					["Name"] = "descendant",
+					["Type"] = "Instance"
+				}
+			}
+		},
+		["Changed"] = {
+			["Arguments"] = {
+				[1] = {
+					["Name"] = "property",
+					["Type"] = "Property"
+				}
+			}
+		},
+		["ChildRemoved"] = {
+			["Arguments"] = {
+				[1] = {
+					["Name"] = "child",
+					["Type"] = "Instance"
+				}
+			}
+		},
+		["ChildAdded"] = {
+			["Arguments"] = {
+				[1] = {
+					["Name"] = "child",
+					["Type"] = "Instance"
+				}
+			}
+		}
+	},
+	["Functions"] = {
+		["ClearAllChildren"] = {
+			["ReturnType"] = "void",
+			["Arguments"] = {}
+		},
+		["FindFirstChild"] = {
+			["ReturnType"] = "Instance",
+			["Arguments"] = {
+				[1] = {
+					["Type"] = "string",
+					["Name"] = "name"
+				},
+				[2] = {
+					["Type"] = "bool",
+					["Name"] = "recursive",
+					["Default"] = "false"
+				}
+			}
+		},
+		["GetDescendants"] = {
+			["ReturnType"] = "Array",
+			["Arguments"] = {}
+		},
+		["GetChildren"] = {
+			["ReturnType"] = "{Instance}",
+			["Arguments"] = {}
+		},
+		["FindFirstAncestorOfClass"] = {
+			["ReturnType"] = "Instance",
+			["Arguments"] = {
+				[1] = {
+					["Type"] = "string",
+					["Name"] = "className"
+				}
+			}
+		},
+		["remove"] = {
+			["ReturnType"] = "void",
+			["Arguments"] = {}
+		},
+		["isDescendantOf"] = {
+			["ReturnType"] = "bool",
+			["Arguments"] = {
+				[1] = {
+					["Type"] = "Instance",
+					["Name"] = "ancestor"
+				}
+			}
+		},
+		["isA"] = {
+			["ReturnType"] = "bool",
+			["Arguments"] = {
+				[1] = {
+					["Type"] = "string",
+					["Name"] = "className"
+				}
+			}
+		},
+		["IsDescendantOf"] = {
+			["ReturnType"] = "bool",
+			["Arguments"] = {
+				[1] = {
+					["Type"] = "Instance",
+					["Name"] = "ancestor"
+				}
+			}
+		},
+		["children"] = {
+			["ReturnType"] = "{Instance}",
+			["Arguments"] = {}
+		},
+		["getChildren"] = {
+			["ReturnType"] = "{Instance}",
+			["Arguments"] = {}
+		},
+		["Destroy"] = {
+			["ReturnType"] = "void",
+			["Arguments"] = {}
+		},
+		["findFirstChild"] = {
+			["ReturnType"] = "Instance",
+			["Arguments"] = {
+				[1] = {
+					["Type"] = "string",
+					["Name"] = "name"
+				},
+				[2] = {
+					["Type"] = "bool",
+					["Name"] = "recursive",
+					["Default"] = "false"
+				}
+			}
+		},
+		["GetFullName"] = {
+			["ReturnType"] = "string",
+			["Arguments"] = {}
+		},
+		["WaitForChild"] = {
+			["ReturnType"] = "Instance",
+			["Arguments"] = {
+				[1] = {
+					["Type"] = "string",
+					["Name"] = "childName"
+				},
+				[2] = {
+					["Type"] = "double",
+					["Name"] = "timeOut"
+				}
+			}
+		},
+		["GetPropertyChangedSignal"] = {
+			["ReturnType"] = "EventInstance",
+			["Arguments"] = {
+				[1] = {
+					["Type"] = "string",
+					["Name"] = "property"
+				}
+			}
+		},
+		["destroy"] = {
+			["ReturnType"] = "void",
+			["Arguments"] = {}
+		},
+		["clone"] = {
+			["ReturnType"] = "Instance",
+			["Arguments"] = {}
+		},
+		["Remove"] = {
+			["ReturnType"] = "void",
+			["Arguments"] = {}
+		},
+		["FindFirstChildOfClass"] = {
+			["ReturnType"] = "Instance",
+			["Arguments"] = {
+				[1] = {
+					["Type"] = "string",
+					["Name"] = "className"
+				}
+			}
+		},
+		["IsAncestorOf"] = {
+			["ReturnType"] = "bool",
+			["Arguments"] = {
+				[1] = {
+					["Type"] = "Instance",
+					["Name"] = "descendant"
+				}
+			}
+		},
+		["Clone"] = {
+			["ReturnType"] = "Instance",
+			["Arguments"] = {}
+		},
+		["FindFirstChildWhichIsA"] = {
+			["ReturnType"] = "Instance",
+			["Arguments"] = {
+				[1] = {
+					["Type"] = "string",
+					["Name"] = "className"
+				},
+				[2] = {
+					["Type"] = "bool",
+					["Name"] = "recursive",
+					["Default"] = "false"
+				}
+			}
+		},
+		["FindFirstAncestor"] = {
+			["ReturnType"] = "Instance",
+			["Arguments"] = {
+				[1] = {
+					["Type"] = "string",
+					["Name"] = "name"
+				}
+			}
+		},
+		["GetDebugId"] = {
+			["ReturnType"] = "string",
+			["Arguments"] = {
+				[1] = {
+					["Type"] = "int",
+					["Name"] = "scopeLength",
+					["Default"] = "4"
+				}
+			}
+		},
+		["IsA"] = {
+			["ReturnType"] = "bool",
+			["Arguments"] = {
+				[1] = {
+					["Type"] = "string",
+					["Name"] = "className"
+				}
+			}
+		},
+		["FindFirstAncestorWhichIsA"] = {
+			["ReturnType"] = "Instance",
+			["Arguments"] = {
+				[1] = {
+					["Type"] = "string",
+					["Name"] = "className"
+				}
+			}
+		}
+	}
+}
+]]--
+
 local returnData = {}
 local Data = game.HttpService:JSONDecode(game.HttpService:GetAsync("https://anaminus.github.io/rbx/json/api/latest.json"))
 
@@ -7,6 +308,9 @@ for _,data in pairs(Data) do
 		if data.Superclass == "Instance" and data.type == "Class" then
 			returnData[data.Name] = returnData[data.Name] or {}
 			returnData[data.Name].Tags = data.Tags
+		elseif data.type == "Class" and not data.Superclass then
+			returnData[data.Name] = returnData[data.Name] or {}
+			returnData[data.Name].Tags = data.tags
 		elseif data.type == "Property" then
 			returnData[data.Class] = returnData[data.Class] or {}
 			returnData[data.Class].Properties = returnData[data.Class].Properties or {}
