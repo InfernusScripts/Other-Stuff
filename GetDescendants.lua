43c4806459f537aea1b6bfc2796d54712e9c7a47
Infernus Scripts <139264639+InfernusScripts@users.noreply.github.com>
2024-11-11 20:20:10 +0300

    Update GetDescendants.lua

--- GetDescendants.lua
+++ GetDescendants.lua
@@ -2,11 +2,11 @@ function GetDsc(obj, ct)
   local c = ct or {}
   for i,v in pairs(obj:GetChildren()) do -- using pairs cuz some stoopid luavms dont support straight iterations
     c[#c+1] = v
-    pcall(GetDsc, v, c)
+    pcall(GetDsc, v, c) -- pcall cuz yk, roblox broke getdescendants
   end
 end
 
 return function(obj)
-  local s,e = pcall(GetDsc, obj or game)
+  local s,e = pcall(GetDsc, obj or game) -- same as on line 5
   return s and e or {}
 end
