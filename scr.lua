9abb0261b71ab2bd8906a3694a0b2de21c5a3b97
Tixannnm <133684746+Tixannnm@users.noreply.github.com>
2025-02-25 16:30:54 +0200

    Update scr.lua

--- scr.lua
+++ scr.lua
@@ -137,7 +137,7 @@ end})
 
 local page2 = window:AddPage({Title = "Misc"})
 
-page:AddToggle({Caption = "Jesus", Callback = function(bool)
+page2:AddToggle({Caption = "Jesus", Callback = function(bool)
 	if bool then
 		for i,v in workspace.Water:GetDescendants() do
 			if v.Name == "Ocean" then
@@ -153,8 +153,59 @@ page:AddToggle({Caption = "Jesus", Callback = function(bool)
 	end
 end, Default = false})
 
+page2:AddToggle({Caption = "Jesus", Callback = function(bool)
+	if bool then
+		while task.wait() do
+			game.Players.LocalPlayer.Character.Head.CanCollide = false
+			game.Players.LocalPlayer.Character.Head.LeftFoot = false
+			game.Players.LocalPlayer.Character.Head.LeftHand = false
+			game.Players.LocalPlayer.Character.Head.LeftLowerArm = false
+			game.Players.LocalPlayer.Character.Head.LeftLowerLeg = false
+			game.Players.LocalPlayer.Character.Head.LeftUpperArm = false
+			game.Players.LocalPlayer.Character.Head.LeftUpperLeg = false
+			game.Players.LocalPlayer.Character.Head.LowerTorso = false
+			game.Players.LocalPlayer.Character.Head.RightFoot = false
+			game.Players.LocalPlayer.Character.Head.RightHand = false
+			game.Players.LocalPlayer.Character.Head.RightLowerArm = false
+			game.Players.LocalPlayer.Character.Head.RightLowerLeg = false
+			game.Players.LocalPlayer.Character.Head.RightUpperArm = false
+			game.Players.LocalPlayer.Character.Head.RightUpperLeg = false
+			game.Players.LocalPlayer.Character.Head.UpperTorso = false
+			game.Players.LocalPlayer.Character.Head.HumanoidRootPart = false
+		end
+	else
+		while task.wait() do
+			game.Players.LocalPlayer.Character.Head.CanCollide = true
+			game.Players.LocalPlayer.Character.Head.LeftFoot = true
+			game.Players.LocalPlayer.Character.Head.LeftHand = true
+			game.Players.LocalPlayer.Character.Head.LeftLowerArm = true
+			game.Players.LocalPlayer.Character.Head.LeftLowerLeg = true
+			game.Players.LocalPlayer.Character.Head.LeftUpperArm = true
+			game.Players.LocalPlayer.Character.Head.LeftUpperLeg = true
+			game.Players.LocalPlayer.Character.Head.LowerTorso = true
+			game.Players.LocalPlayer.Character.Head.RightFoot = true
+			game.Players.LocalPlayer.Character.Head.RightHand = true
+			game.Players.LocalPlayer.Character.Head.RightLowerArm = true
+			game.Players.LocalPlayer.Character.Head.RightLowerLeg = true
+			game.Players.LocalPlayer.Character.Head.RightUpperArm = true
+			game.Players.LocalPlayer.Character.Head.RightUpperLeg = true
+			game.Players.LocalPlayer.Character.Head.UpperTorso = true
+			game.Players.LocalPlayer.Character.Head.HumanoidRootPart = true
+		end
+	end
+end, Default = false})
+
 local button = page2:AddButton({Caption = "Delete all Lasers", Callback = function()
 	workspace.RandomModels.DeathLights:Destroy()
+	workspace.Casino.CasinoDoor.SpinLaser1:Destroy()
+	workspace.Casino.CasinoDoor.SpinLaser2:Destroy()
+	workspace.Casino.CasinoDoor.SpinLaser3:Destroy()
+	for i,v in workspace.Ignore.WorldObjects:GetDescendants() do
+		if v.Name == "Lasers" then
+			v:Destroy()
+		end
+	end
+	
 end})
 
 local button = page2:AddButton({Caption = "Outfits", Callback = function()
